cb822591f0c01b6b41137c095f1467844be5e63a
tom.hu <tom.hu@guanaitong.com>
2022-07-14 14:04:00 +0800

    优化通知json解析错误

--- src/main/java/com/gat/open/sdk/model/notify/NotifyEnterpriseCharge.java
+++ src/main/java/com/gat/open/sdk/model/notify/NotifyEnterpriseCharge.java
@@ -5,23 +5,21 @@
 
 package com.gat.open.sdk.model.notify;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSetter;
 
 import java.math.BigDecimal;
 
 public class NotifyEnterpriseCharge extends NotifyBase {
 
     @JsonProperty("account_openid")
-    String accountOpenid;
+    private String accountOpenid;
 
     @JsonProperty("charge_amount")
-    BigDecimal chargeAmount;
-
-    BigDecimal balance;
-
-    String chargeTime;
+    private BigDecimal chargeAmount;
+    @JsonProperty("balance")
+    private BigDecimal balance;
+    @JsonProperty("charge_time")
+    private String chargeTime;
 
     public String getAccountOpenid() {
         return accountOpenid;
@@ -39,22 +37,18 @@ public class NotifyEnterpriseCharge extends NotifyBase {
         this.chargeAmount = chargeAmount;
     }
 
-    @JsonGetter("total_amount")
     public BigDecimal getBalance() {
         return balance;
     }
 
-    @JsonSetter("balance")
     public void setBalance(BigDecimal balance) {
         this.balance = balance;
     }
 
-    @JsonGetter("charge_at")
     public String getChargeTime() {
         return chargeTime;
     }
 
-    @JsonSetter("charge_time")
     public void setChargeTime(String chargeTime) {
         this.chargeTime = chargeTime;
     }
