09fd6db46042398807083c2f7237ec472c6646f1
tom.hu <tom.hu@guanaitong.com>
2023-06-06 13:46:13 +0800

    查询账单集接口提交

---
+++ src/main/java/com/gat/open/sdk/model/expense/FindBillAggRequest.java
@@ -0,0 +1,57 @@
+package com.gat.open.sdk.model.expense;
+
+import com.gat.open.sdk.model.EnterpriseCodeRequest;
+import com.gat.open.sdk.model.FormRequest;
+import com.gat.open.sdk.model.Pagination;
+
+/**
+ * https://open.guanaitong.com/doc/enterprise/expense/bill-agg-find/
+ */
+public class FindBillAggRequest extends FormRequest<Pagination<FindBillAggResponse>> implements EnterpriseCodeRequest {
+
+    private String enterpriseCode;
+    private String timeStart;
+    private String timeEnd;
+    private Integer pageNo = 1;
+    private Integer pageSize = 20;
+
+    public String getEnterpriseCode() {
+        return enterpriseCode;
+    }
+
+    public void setEnterpriseCode(String enterpriseCode) {
+        this.enterpriseCode = enterpriseCode;
+    }
+
+    public String getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(String timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public String getTimeEnd() {
+        return timeEnd;
+    }
+
+    public void setTimeEnd(String timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
---
+++ src/test/java/com/gat/open/sdk/client/ExpenseApiTest.java
@@ -0,0 +1,22 @@
+package com.gat.open.sdk.client;
+
+import com.gat.open.sdk.model.Pagination;
+import com.gat.open.sdk.model.expense.FindBillAggRequest;
+import com.gat.open.sdk.model.expense.FindBillAggResponse;
+import com.gat.open.sdk.util.JSON;
+import org.junit.Test;
+
+public class ExpenseApiTest {
+    private OpenClient openClient = Clients.openClient;
+
+    @Test
+    public void testBillAggFind() {
+        FindBillAggRequest findBillAggRequest = new FindBillAggRequest();
+        findBillAggRequest.setTimeStart("2023-03-31 20:39:00");
+        findBillAggRequest.setTimeEnd("2023-03-31 20:40:00");
+        findBillAggRequest.setPageSize(50);
+        findBillAggRequest.setPageNo(1);
+        Pagination<FindBillAggResponse> billAgg = openClient.expenseApi().findBillAgg(findBillAggRequest);
+        System.out.println(JSON.toJSONString(billAgg));
+    }
+}
