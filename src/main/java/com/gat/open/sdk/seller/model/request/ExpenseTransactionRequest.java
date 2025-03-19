7e081568d32b16f72ce43c4d972d68b1c3cf306c
赵帅 <shuai.zhao@guanaitong.com>
2025-03-19 14:50:52 +0800

    新增商家账户对账流水接口

---
+++ src/main/java/com/gat/open/sdk/seller/model/request/ExpenseTransactionRequest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2007-2023, CIIC Guanaitong, Co., Ltd.
+ * All rights reserved.
+ */
+
+package com.gat.open.sdk.seller.model.request;
+
+import com.gat.open.sdk.model.JsonRequest;
+import com.gat.open.sdk.model.Pagination;
+import com.gat.open.sdk.model.Pagination2;
+import com.gat.open.sdk.seller.model.response.ExpenseTransactionResponse;
+
+/**
+ * @author shuai.zhao@guaniatong.com
+ */
+public class ExpenseTransactionRequest extends JsonRequest<Pagination2<ExpenseTransactionResponse>> {
+    /**
+     * 查询区间的起始时间，格式：yyyy-MM-dd HH:mm:ss。默认：3天前0点。
+     */
+    private String timeCreatedStart;
+    /**
+     * 查询区间的结束时间，格式同上。默认当前时间。
+     */
+    private String timeCreatedEnd;
+    /**
+     * 交易所属应用清单，使用”,“拼接。
+     */
+    private String sourceAppIds;
+
+    /**
+     * 关爱通的退款交易号。
+     */
+    private String refundTradeNo;
+
+    /**
+     * 关爱通订单号（对应收入项的订单号)
+     */
+    private String outerTradeNo;
+    /**
+     * 当前页码，默认 1。
+     */
+    private Integer page = 1;
+    /**
+     * 每页记录数，默认 20。min:1, max:100。
+     */
+    private Integer rowsPerPage = 20;
+    /**
+     * 排序模式（按交易发生时间），升序 or 降序，默认：降序。
+     */
+    private Boolean sortMode = Boolean.FALSE;
+
+    public String getTimeCreatedStart() {
+        return timeCreatedStart;
+    }
+
+    public void setTimeCreatedStart(String timeCreatedStart) {
+        this.timeCreatedStart = timeCreatedStart;
+    }
+
+    public String getTimeCreatedEnd() {
+        return timeCreatedEnd;
+    }
+
+    public void setTimeCreatedEnd(String timeCreatedEnd) {
+        this.timeCreatedEnd = timeCreatedEnd;
+    }
+
+    public String getSourceAppIds() {
+        return sourceAppIds;
+    }
+
+    public void setSourceAppIds(String sourceAppIds) {
+        this.sourceAppIds = sourceAppIds;
+    }
+
+    public String getRefundTradeNo() {
+        return refundTradeNo;
+    }
+
+    public void setRefundTradeNo(String refundTradeNo) {
+        this.refundTradeNo = refundTradeNo;
+    }
+
+    public String getOuterTradeNo() {
+        return outerTradeNo;
+    }
+
+    public void setOuterTradeNo(String outerTradeNo) {
+        this.outerTradeNo = outerTradeNo;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRowsPerPage() {
+        return rowsPerPage;
+    }
+
+    public void setRowsPerPage(Integer rowsPerPage) {
+        this.rowsPerPage = rowsPerPage;
+    }
+
+    public Boolean getSortMode() {
+        return sortMode;
+    }
+
+    public void setSortMode(Boolean sortMode) {
+        this.sortMode = sortMode;
+    }
+}
---
+++ src/main/java/com/gat/open/sdk/seller/model/response/IncomeTransactionResponse.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2007-2023, CIIC Guanaitong, Co., Ltd.
+ * All rights reserved.
+ */
+
+package com.gat.open.sdk.seller.model.response;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.math.BigDecimal;
+
+/**
+ * @author shuai.zhao@guaniatong.com
+ */
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
+public class IncomeTransactionResponse {
+
+    /**
+     * 交易发生时间。格式：yyyy-MM-dd HH:mm:ss
+     */
+    private String timeCreated;
+    /**
+     * 关爱通订单号。默认是：appId + 供应商订单号。
+     */
+    private String outerTradeNo;
+
+    /**
+     * 交易流水号。
+     */
+    private String tradeNo;
+    /**
+     * 交易来源应用。
+     */
+    private String sourceAppId;
+    /**
+     * 积分收入。
+     */
+    private BigDecimal usePoint;
+    /**
+     * 现金收入。
+     */
+    private BigDecimal useCash;
+    /**
+     * 交易备注。
+     */
+    private String remark;
+
+    public String getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(String timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public String getOuterTradeNo() {
+        return outerTradeNo;
+    }
+
+    public void setOuterTradeNo(String outerTradeNo) {
+        this.outerTradeNo = outerTradeNo;
+    }
+
+    public String getTradeNo() {
+        return tradeNo;
+    }
+
+    public void setTradeNo(String tradeNo) {
+        this.tradeNo = tradeNo;
+    }
+
+    public String getSourceAppId() {
+        return sourceAppId;
+    }
+
+    public void setSourceAppId(String sourceAppId) {
+        this.sourceAppId = sourceAppId;
+    }
+
+    public BigDecimal getUsePoint() {
+        return usePoint;
+    }
+
+    public void setUsePoint(BigDecimal usePoint) {
+        this.usePoint = usePoint;
+    }
+
+    public BigDecimal getUseCash() {
+        return useCash;
+    }
+
+    public void setUseCash(BigDecimal useCash) {
+        this.useCash = useCash;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
